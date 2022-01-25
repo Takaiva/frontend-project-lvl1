7599b353ee264084b959734a7e3006ff4fed4656
Александр Сдобников <sdobnikove@mail.ru>
2022-01-25 05:15:39 +0300

    add src/cli.js with readline-sync package

---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from "readline-sync";
+console.log('Welcome to the Brain Games!');
+export const userName = readlineSync.question("May I have your name? ");
+console.log(`Hello, ${userName}!`);
+
