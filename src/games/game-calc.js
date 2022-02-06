9087ea59d5098c5ce6b14f22c476243ababb9684
Александр Сдобников <sdobnikove@mail.ru>
2022-02-07 00:29:00 +0300

    add needed game logic

---
+++ src/games/game-calc.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+import randomNum from '../functions/randomNum.js';
+import randomItem from '../functions/randomItem.js';
+import expressionResult from '../functions/expressionResult.js';
+
+const gameCalc = () => {
+  const operators = ['+', '-', '*'];
+  let roundCount = 0;
+  for (; roundCount < 3; roundCount += 1) {
+    const number1 = randomNum(1, 100);
+    const number2 = randomNum(1, 100);
+    const operator = randomItem(operators);
+    console.log(`Question: ${number1} ${operator} ${number2}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (Number(userAnswer) === expressionResult(number1, number2, operator)) {
+      console.log('Correct!');
+    } else {
+      console.log(
+        `"${userAnswer}" is wrong answer ;(. Correct answer was "${expressionResult(
+          number1,
+          number2,
+          operator,
+        )}".`,
+      );
+      return false;
+    }
+  }
+  return true;
+};
+
+export default gameCalc;
