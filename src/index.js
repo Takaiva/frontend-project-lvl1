84433d516d98aca4d21ff2f462c968fd6a5c88c7
Александр Сдобников <sdobnikove@mail.ru>
2022-02-06 04:56:25 +0300

    add general game logic and make src/index.js the main game execution file

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import readlineSync from 'readline-sync';
+
+const game = (typeOfGame, rule) => {
+  console.log('Welcome to the Brain Games!');
+
+  const userName = readlineSync.question('May I have your name? ');
+  const success = (`Congratulations, ${userName}!`);
+  const fail = (`Let's try again, ${userName}`);
+
+  console.log(`Hello, ${userName}!`);
+
+  console.log(rule);
+
+  if (typeOfGame()) {
+    return console.log(success);
+  }
+  return console.log(fail);
+};
+
+export default game;
