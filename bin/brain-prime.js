19da29fe0132cd1def7ea2bfad4d616b136fcff7
Александр Сдобников <sdobnikove@mail.ru>
2022-02-08 02:41:46 +0300

    fix a mistake

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -3,4 +3,4 @@
 import game from '../src/index.js';
 import gamePrime from '../src/games/game-prime.js';
 
-game(gamePrime, 'Answer"yes" if given number is prime. Otherwise answer "no".');
+game(gamePrime, 'Answer "yes" if given number is prime. Otherwise answer "no".');
