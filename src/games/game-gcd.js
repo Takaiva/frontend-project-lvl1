bb7c3199438e7ac0a7e057be802ce1ec2d1f4973
Александр Сдобников <sdobnikove@mail.ru>
2022-02-07 04:48:09 +0300

    add needed game logic

---
+++ src/games/game-gcd.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+import randomNum from '../functions/randomNum.js';
+import gcd from '../functions/gcd.js';
+
+const gameGcd = () => {
+  let roundCount = 0;
+  for (; roundCount < 3; roundCount += 1) {
+    const number1 = randomNum(1, 100);
+    const number2 = randomNum(1, 100);
+    const correctAnswer = gcd(number1, number2);
+    console.log(`Question: ${number1} ${number2}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (gcd(number1, number2) === userAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(
+        `"${userAnswer}" is wrong answer. Correct answer was ${correctAnswer}`,
+      );
+      return false;
+    }
+  }
+  return true;
+};
+
+export default gameGcd;
