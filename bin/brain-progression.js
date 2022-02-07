dc1a768383d376cb91744f2f037259944fe0f5b3
Александр Сдобников <sdobnikove@mail.ru>
2022-02-07 23:08:17 +0300

    add needed imports and game function

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
+import game from '../src/index.js';
+import gameProgression from '../src/games/game-progression.js';
 
+game(gameProgression, 'What number is missing in the progression?');
