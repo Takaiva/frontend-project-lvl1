aa413e84150b7ea812cebf61c637b41eddc7f099
Александр Сдобников <sdobnikove@mail.ru>
2022-02-08 02:36:23 +0300

    add brain-prime.js, needed imports, start game function

---
+++ bin/brain-prime.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import game from '../src/index.js';
+import gamePrime from '../src/games/game-prime.js';
+
+game(gamePrime, 'Answer"yes" if given number is prime. Otherwise answer "no".');
