85175715e99aff67829da2c9f9bb7ee762967230
Александр Сдобников <sdobnikove@mail.ru>
2022-02-11 04:21:37 +0300

    update isPrime.js

--- src/functions/isPrime.js
+++ src/functions/isPrime.js
@@ -2,9 +2,6 @@ const isPrime = (num) => {
   if (num <= 1) {
     return false;
   }
-  if (num === 2) {
-    return true;
-  }
   let flag = 0;
   for (let i = 2; i <= num / 2 + 1; i += 1) {
     if (num % i === 0) {
