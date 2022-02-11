d04bfb9a50fa869ce8ffbeb6d18123adba8188b8
Александр Сдобников <sdobnikove@mail.ru>
2022-02-11 04:15:39 +0300

    update general game logic for all the games

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 import readlineSync from 'readline-sync';
 
-const game = (typeOfGame, rule) => {
+const game = (dataOfGame, rule) => {
+  const roundsCount = 3;
+
   console.log('Welcome to the Brain Games!');
 
   const userName = readlineSync.question('May I have your name? ');
-  const success = (`Congratulations, ${userName}!`);
-  const fail = (`Let's try again, ${userName}!`);
+  const success = `Congratulations, ${userName}!`;
+  const fail = `Let's try again, ${userName}!`;
 
   console.log(`Hello, ${userName}!`);
 
   console.log(rule);
 
-  if (typeOfGame()) {
-    return console.log(success);
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [num, correctAnswer] = dataOfGame();
+
+    console.log(`Question: ${num}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(
+        `"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`,
+      );
+      console.log(fail);
+      return false;
+    }
   }
-  return console.log(fail);
+  console.log(success);
+  return true;
 };
 
 export default game;
