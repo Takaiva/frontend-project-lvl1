1ad82c6c1527689643c973a0ed3efb053d591473
Александр Сдобников <sdobnikove@mail.ru>
2022-02-16 22:59:34 +0300

    update isPrime.js

--- src/functions/isPrime.js
+++ src/functions/isPrime.js
@@ -2,7 +2,6 @@ const isPrime = (num) => {
   const sqrtnum = Math.floor(Math.sqrt(num));
   let prime = num !== 1;
   for (let i = 2; i < sqrtnum + 1; i += 1) {
-    // sqrtnum+1
     if (num % i === 0) {
       prime = false;
       break;
