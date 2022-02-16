7967350a6f913c69d9201c90afa909c1ac0ecc5e
Александр Сдобников <sdobnikove@mail.ru>
2022-02-16 23:00:37 +0300

    add updated getRightAnswer.js function

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,9 +1,10 @@
 import getRightAnswer from '../functions/rightAnswer.js';
 import getRandomNum from '../functions/randomNum.js';
+import isEven from '../functions/isEven.js';
 
 const gameEven = () => {
   const num = getRandomNum(1, 100);
-  const correctAnswer = getRightAnswer(num);
+  const correctAnswer = getRightAnswer(num, isEven);
   return [num, correctAnswer];
 };
 
