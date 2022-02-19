c9d90d68bc583752225df2e51e0de801d31a55d7
Александр Сдобников <sdobnikove@mail.ru>
2022-02-19 17:46:56 +0300

    simplify code logic and add "rule" export

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,28 +1,14 @@
 import getRandomNum from '../functions/randomNum.js';
-import getRandomItem from '../functions/randomItem.js';
 import isPrime from '../functions/isPrime.js';
 import getRightAnswer from '../functions/rightAnswer.js';
 
-const gamePrime = () => {
-  const primeNumbers = [
-    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-    73, 79, 83, 89, 97,
-  ];
+const rule = 'Answer "yes" if the given number is prime. Otherwise answer "no".';
 
-  const random = ['Prime', 'NotPrime'];
-  let number = 0;
-  const primeOrNot = getRandomItem(random);
-  if (primeOrNot === 'Prime') {
-    number = getRandomItem(primeNumbers);
-  } else {
-    number = getRandomNum(2, 100);
-    while (isPrime(number) === true) {
-      number = getRandomNum(2, 100);
-    }
-  }
+const gamePrime = () => {
+  const number = getRandomNum(2, 100);
   const question = `${number}`;
   const correctAnswer = getRightAnswer(number, isPrime);
   return [question, correctAnswer];
 };
 
-export default gamePrime;
+export { gamePrime, rule };
